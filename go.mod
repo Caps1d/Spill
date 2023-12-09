d155ae7601b3a59620b57a3291e17f4c09346e96
yegor smertenko <ye.smertenko@gmail.com>
2023-12-08 16:05:52 -0800

    refactor code, use chi router, json response

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.21.0
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.0.10 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,90 @@
 package main
 
 import (
-	"context"
+	// "context"
 	"database/sql"
+	"encoding/json"
+	// "encoding/json"
 	"fmt"
-	"html"
 	"log"
-	"net"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	// "html"
+	// "log"
+	// "net"
 	"net/http"
-	"strconv"
+	// "strconv"
 	"time"
 
 	_ "github.com/lib/pq"
 )
+
+// getPosts func
+func getPosts(w http.ResponseWriter, r *http.Request, conn *sql.DB) {
+	// w.Write([]byte("Calling the db and getting the posts:\n"))
+	fmt.Println("Calling the db and getting the posts")
+
+	rows, err := conn.Query("SELECT title, content, userid, createdat FROM post;")
+
+	if err != nil {
+		log.Printf("Error while getting rows from the db, err: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error"))
+	}
+
+	type Post struct {
+		Title     string `json:"title"`
+		Content   string `json:"content"`
+		UserId    int    `json:"userid"`
+		CreatedAt string `json:"createdat"`
+	}
+
+	// array to store posts
+	var posts []Post
+
+	for rows.Next() {
+		var post Post
+
+		// here its the same, just post.title, post.content,..
+		if err := rows.Scan(&post.Title, &post.Content, &post.UserId, &post.CreatedAt); err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		//append that struct into the posts array
+		posts = append(posts, post)
+
+		// formattedString := fmt.Sprintf("title: %s\ncontent: %s\nwritten by: %d\ndate: %s\n\n", title, content, userid, createdat)
+		// w.Write([]byte(formattedString))
+	}
+	// for testing
+	fmt.Printf("Posts array: %v", posts)
+
+	// compose the json with the posts array
+	js, err := json.Marshal(posts)
+
+	if err != nil {
+		fmt.Printf("Error while composing json, err: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Internal Server Error =("))
+	}
+
+	js = append(js, '\n')
+
+	if err := rows.Err(); err != nil {
+		log.Printf("Could not retrieve the row, err = %s", err)
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+	rows.Close()
+}
+
+func addPost(w http.ResponseWriter, r *http.Request, conn *sql.DB) {
+
+}
 
 func main() {
 	//Note: checkout JSON marshall Go
@@ -33,111 +105,129 @@ func main() {
 	conn.SetMaxOpenConns(10)
 	conn.SetMaxIdleConns(10)
 
-	// home route handler
-	http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
-		// will output a simple html text line saying:
-		// "Welcome to /home"
-		fmt.Fprint(w, "Welcome to ", html.EscapeString(r.URL.Path))
-		// fmt.Fprint(w, "Welcome, to my server!")
+	// router setup with chi
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/home", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Welcome"))
 	})
-	log.Println("Server starting...")
 
-	listener, err := net.Listen("tcp", ":8080")
-
-	if err == nil {
-		fmt.Println("Server is online, listening on port:8080")
-	} else {
-		log.Fatal(err)
-	}
-
-	// posts route handler: only GET and POST requests
-	// get rows from db post table and handle get requests sent to post route
-	http.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
-		if r.URL.Path != "/posts" {
-			http.Error(w, "404 not found.", http.StatusNotFound)
-		}
-
-		switch r.Method {
-		case "GET":
-			rows, err := conn.Query("SELECT * FROM post;")
-			if err != nil {
-				panic(err)
-			}
-
-			for rows.Next() {
-				var id int
-				var title, content, date string
-				var authorId int
-
-				if err := rows.Scan(&id, &title, &content, &authorId, &date); err != nil {
-					fmt.Println(err)
-					continue
-				}
-				fmt.Printf(" ID: %d,\n Title: %s,\n Content: %s,\n AuthorId: %d,\n Date: %s\n", id, title, content, authorId, date)
-
-				// display the returned rows
-				fmt.Fprintf(w, " ID: %d,\n Title: %s,\n Content: %s,\n AuthorId: %d,\n Date: %s\n", id, title, content, authorId, date)
-			}
-
-			// handle errors from rows.Next()
-			if err := rows.Err(); err != nil {
-				fmt.Println("Error during iterating over rows:", err)
-			}
-
-			// close rows explicitly
-			if err := rows.Close(); err != nil {
-				fmt.Println("Error closing rows:", err)
-			}
-
-		case "POST":
-			if err := r.ParseForm(); err != nil {
-				fmt.Fprintf(w, "ParseForm() err: %v", err)
-				return
-			}
-
-			fmt.Fprintf(w, "Post from website, r.PostForm = %v\n", r.PostForm)
-
-			title := r.FormValue("title")
-			content := r.FormValue("content")
-			useridString := r.FormValue("userid")
-
-			// Convert the string to int64 because post table expects an int
-			userid, err := strconv.ParseInt(useridString, 10, 64)
-			if err != nil {
-				// Handle the error (e.g., invalid input)
-				log.Printf("Error converting userid to int64: %s", err)
-			}
-			// printing the FormValue's
-			fmt.Fprintf(w, "Post title: %s is of type %T\n", title, title)
-			fmt.Fprintf(w, "Content: %s is of type %T\n", content, content)
-			fmt.Fprintf(w, "Created by: %d is of type %T\n", userid, userid)
-
-			query := "INSERT INTO post (title, content, userId) VALUES ($1, $2, $3) Returning id;"
-			// QueryRowContext runs a query that returns 1 row at most
-			row := conn.QueryRowContext(context.Background(), query, title, content, userid)
-
-			// since post table's pk is serial, its useful to keep track of the last
-			// inserted key
-			var insertedID int64
-			err = row.Scan(&insertedID)
-			if err != nil {
-				if err == sql.ErrNoRows {
-					log.Println("No rows were inserted.")
-				} else {
-					log.Fatalf("Could not retrieve last inserted id: %s", err)
-				}
-			} else {
-				fmt.Printf("Inserted id: %d", insertedID)
-			}
-
-		default:
-			fmt.Fprintf(w, "Sorry, this router only handles GET and POST requests ='(")
-		}
-
+	r.Get("/posts", func(w http.ResponseWriter, r *http.Request) {
+		getPosts(w, r, conn)
 	})
-	// we don't need to specify a handler since we registered a handler in HandleFunc 2nd arg
-	// hence we pass nil
-	http.Serve(listener, nil)
+
+	r.Post("/posts", func(w http.ResponseWriter, r *http.Request) {
+		addPost(w, r, conn)
+	})
+
+	http.ListenAndServe(":8080", r)
+
+	// home route handler - standard lib setup
+	// http.HandleFunc("/home", func(w http.ResponseWriter, r *http.Request) {
+	// 	// will output a simple html text line saying:
+	// 	// "Welcome to /home"
+	// 	fmt.Fprint(w, "Welcome to ", html.EscapeString(r.URL.Path))
+	// 	// fmt.Fprint(w, "Welcome, to my server!")
+	// })
+	// log.Println("Server starting...")
+	//
+	// listener, err := net.Listen("tcp", ":8080")
+	//
+	// if err == nil {
+	// 	fmt.Println("Server is online, listening on port:8080")
+	// } else {
+	// 	log.Fatal(err)
+	// }
+	//
+	// // posts route handler: only GET and POST requests
+	// // get rows from db post table and handle get requests sent to post route
+	// // this is a standard lib setup
+	// http.HandleFunc("/posts", func(w http.ResponseWriter, r *http.Request) {
+	// 	if r.URL.Path != "/posts" {
+	// 		http.Error(w, "404 not found.", http.StatusNotFound)
+	// 	}
+	//
+	// 	switch r.Method {
+	// 	case "GET":
+	// 		rows, err := conn.Query("SELECT * FROM post;")
+	// 		if err != nil {
+	// 			panic(err)
+	// 		}
+	//
+	// 		for rows.Next() {
+	// 			var id int
+	// 			var title, content, date string
+	// 			var authorId int
+	//
+	// 			if err := rows.Scan(&id, &title, &content, &authorId, &date); err != nil {
+	// 				fmt.Println(err)
+	// 				continue
+	// 			}
+	// 			fmt.Printf(" ID: %d,\n Title: %s,\n Content: %s,\n AuthorId: %d,\n Date: %s\n", id, title, content, authorId, date)
+	//
+	// 			// display the returned rows
+	// 			fmt.Fprintf(w, " ID: %d,\n Title: %s,\n Content: %s,\n AuthorId: %d,\n Date: %s\n", id, title, content, authorId, date)
+	// 		}
+	//
+	// 		// handle errors from rows.Next()
+	// 		if err := rows.Err(); err != nil {
+	// 			fmt.Println("Error during iterating over rows:", err)
+	// 		}
+	//
+	// 		// close rows explicitly
+	// 		if err := rows.Close(); err != nil {
+	// 			fmt.Println("Error closing rows:", err)
+	// 		}
+	//
+	// 	case "POST":
+	// 		if err := r.ParseForm(); err != nil {
+	// 			fmt.Fprintf(w, "ParseForm() err: %v", err)
+	// 			return
+	// 		}
+	//
+	// 		fmt.Fprintf(w, "Post from website, r.PostForm = %v\n", r.PostForm)
+	//
+	// 		title := r.FormValue("title")
+	// 		content := r.FormValue("content")
+	// 		useridString := r.FormValue("userid")
+	//
+	// 		// Convert the string to int64 because post table expects an int
+	// 		userid, err := strconv.ParseInt(useridString, 10, 64)
+	// 		if err != nil {
+	// 			// Handle the error (e.g., invalid input)
+	// 			log.Printf("Error converting userid to int64: %s", err)
+	// 		}
+	// 		// printing the FormValue's
+	// 		fmt.Fprintf(w, "Post title: %s is of type %T\n", title, title)
+	// 		fmt.Fprintf(w, "Content: %s is of type %T\n", content, content)
+	// 		fmt.Fprintf(w, "Created by: %d is of type %T\n", userid, userid)
+	//
+	// 		query := "INSERT INTO post (title, content, userId) VALUES ($1, $2, $3) Returning id;"
+	// 		// QueryRowContext runs a query that returns 1 row at most
+	// 		row := conn.QueryRowContext(context.Background(), query, title, content, userid)
+	//
+	// 		// since post table's pk is serial, its useful to keep track of the last
+	// 		// inserted key
+	// 		var insertedID int64
+	// 		err = row.Scan(&insertedID)
+	// 		if err != nil {
+	// 			if err == sql.ErrNoRows {
+	// 				log.Println("No rows were inserted.")
+	// 			} else {
+	// 				log.Fatalf("Could not retrieve last inserted id: %s", err)
+	// 			}
+	// 		} else {
+	// 			fmt.Printf("Inserted id: %d", insertedID)
+	// 		}
+	//
+	// 	default:
+	// 		fmt.Fprintf(w, "Sorry, this router only handles GET and POST requests ='(")
+	// 	}
+	//
+	// })
+	// // we don't need to specify a handler since we registered a handler in HandleFunc 2nd arg
+	// // hence we pass nil
+	// http.Serve(listener, nil)
 
 	// don't forget to close the db connection
 	conn.Close()
